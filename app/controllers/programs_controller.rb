565ea2e05b7bb3fb7cd7c32bdf7227e05b11e814
Rhea Akiki <rhea.akiki@gmail.com>
2017-06-20 19:33:37 +0200

    ADDED MAPS

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,15 +1,39 @@
 class ProgramsController < ApplicationController
   def show
+
   end
 
   def index
+
   end
 
   def search
+
+
     if params[:search_value].nil?
       @programs = Program.search_modal(params[:subject], params[:level], params[:tuition].to_i)
+            @list = @programs.map { |p| p.university }
+
+
+      @hash = Gmaps4rails.build_markers(@list) do |uni, marker|
+
+        marker.lat uni.latitude
+        marker.lng uni.longitude
+      end
+
     else
-      @programs = Program.search(params[:search_value])
+      @programs = Program.search(params[:search_value]).select do |program|
+        program.university and program.university.latitude and program.university.longitude
+      end
+
+      @list = @programs.map { |p| p.university }
+
+
+      @hash = Gmaps4rails.build_markers(@list) do |uni, marker|
+
+        marker.lat uni.latitude
+        marker.lng uni.longitude
+      end
     end
   end
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,4 +1,6 @@
 class University < ApplicationRecord
   has_many :programs
   validates :name, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :location_changed?
 end
