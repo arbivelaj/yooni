fc2592c45b4b5d5f491a5e800518f257a7461cd8
arbi velaj <arbi.velaj@gmail.com>
2017-06-20 13:49:15 +0200

    routes for programs

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+  resources :programs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
