1a0008a87542a862580f4891e491f516ceb869bb
Rhea Akiki <rhea.akiki@gmail.com>
2017-06-21 19:34:21 +0200

    fix bugs

--- db/migrate/20170621150135_changetype3.rb
+++
@@ -1,9 +0,0 @@
-class Changetype3 < ActiveRecord::Migration[5.0]
-  def up
-    change_column :programs, :scholarship, 'integer USING CAST(scholarship AS integer)'
-  end
-
-  def down
-    change_column :programs, :scholarship, :string
-  end
-end
