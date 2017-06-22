9a9f60c3fe86034edff12a128ff4bf001893a243
arbi velaj <arbi.velaj@gmail.com>
2017-06-22 14:54:15 +0200

    fixed filter - only scholarhip missing

--- app/models/program.rb
+++ app/models/program.rb
@@ -11,18 +11,53 @@ class Program < ApplicationRecord
       self.where("title ILIKE ? OR subject ILIKE ? OR level ILIKE ?", q, q, q)
     end
 
-    def search_modal(subject, level, tuition)
+    def search_modal(subject, level, location, tuit)
       q1 = "%#{subject}%"
       q2 = "%#{level}%"
-      q3 = tuition
+      q3 = "%#{location}%"
+      q4 = tuit
 
-      data = [
-        (subject.present?) ? self.where("subject ILIKE ?", q1) : [],
-        (level.present?) ? self.where("level ILIKE ?", q2) : [],
-        (not tuition.zero?) ? self.where("tuition = ?", q3) : [],
-      ]
+      tuition_array = []
 
-      data.flatten
+
+
+      if q4 == 0
+        array_f = self.where("subject ILIKE ? AND level ILIKE ? ", q1, q2)
+        array_g = University.where("location ILIKE ?", q3).map {|university| university.programs }.flatten
+
+       array = array_f.select{|x| array_g.include?(x)}
+
+      else
+        array_f = self.where("subject ILIKE ? AND level ILIKE ? ", q1, q2)
+        array_g = University.where("location ILIKE ?", q3).map {|university| university.programs }.flatten
+
+        array = array_f.select{|x| array_g.include?(x)}
+        tuition_array << self.where("tuition < ?", q4)
+        arbi = tuition_array.flatten
+         array_final = array.select{|k| arbi.include?(k)}
+
+      end
+
+
+
+
+      # data = [
+      #   (subject.present?) ? self.where("subject ILIKE ?", q1) : [],
+      #   (level.present?) ? self.where("level ILIKE ?", q2) : [],
+      # #    (location.present?) ? University.where("location ILIKE ?", q3).map {|university| university.programs }.flatten : []
+      # #       ]
+
+      # array_f = self.where("subject ILIKE ? AND level ILIKE ? ", q1, q2)
+      # array_g = University.where("location ILIKE ?", q3).map {|university| university.programs }.flatten
+
+      # array = array_f.select{|x| array_g.include?(x)}
+      # raise
+      # if tuition =! 0
+      #   return array_final = array.select{|x| tuition_array.include?(x)}
+      # else
+      #   return array
+      # end
+
     end
 
     def search_city(c)
@@ -30,7 +65,7 @@ class Program < ApplicationRecord
       University.where("location ILIKE ?", cit).map {|university| university.programs }.flatten
     end
 
-     def search_level(l)
+    def search_level(l)
       lev = "%#{l}%"
       self.where("level ILIKE ?", lev)
     end
