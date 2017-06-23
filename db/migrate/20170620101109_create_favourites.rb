6b1899b591a1c3c9463ac797599c3da517012416
arbi velaj <arbi.velaj@gmail.com>
2017-06-23 11:09:00 +0200

    migrate

--- db/migrate/20170620101109_create_favourites.rb
+++ db/migrate/20170620101109_create_favourites.rb
@@ -1,5 +1,19 @@
 class CreateFavourites < ActiveRecord::Migration[5.0]
   def change
+
+    create_table :programs do |t|
+
+      t.string   "title"
+      t.integer  "ranking"
+      t.integer  "tuition"
+      t.integer  "scholarship"
+      t.string   "level"
+      t.string   "subject"
+      t.references :university, foreign_key: true
+
+      t.timestamps
+    end
+
     create_table :favourites do |t|
       t.references :user, foreign_key: true
       t.references :program, foreign_key: true
