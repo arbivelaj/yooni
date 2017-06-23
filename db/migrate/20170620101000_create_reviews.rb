7b4eee0ea4e04fa3d4f23b2ed018bfb3947f883d
arbi velaj <arbi.velaj@gmail.com>
2017-06-23 11:14:10 +0200

    migration

--- db/migrate/20170620101000_create_reviews.rb
+++ db/migrate/20170620101000_create_reviews.rb
@@ -1,5 +1,19 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
+
+    create_table :programs do |t|
+
+      t.string   "title"
+      t.integer  "ranking"
+      t.integer  "tuition"
+      t.integer  "scholarship"
+      t.string   "level"
+      t.string   "subject"
+      t.references :university, foreign_key: true
+
+      t.timestamps
+    end
+
     create_table :reviews do |t|
       t.string :content
       t.references :user, foreign_key: true
